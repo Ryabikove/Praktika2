b2b788445f3e711edfcf4ed667edbe067dc23aa4
Ryabikove <lodogatze@mail.ru>
2026-08-26 20:39:35 +0300

    Now it can read and write into console and file - close #6

--- DatasetAnalysis/dataset.py
+++ DatasetAnalysis/dataset.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('dataset.csv')
 
+def displayData(data, filename):
+    print(data)
+    with (open(filename, 'a', newline='', encoding='utf-8')) as file:
+        file.write("\n" + data)
 
 if __name__ == '__main__':
-    print(df.columns)
+    displayData(df.columns.values[1], 'report.txt')
