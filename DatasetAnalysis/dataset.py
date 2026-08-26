959dc910f3a866f61b13915496581bedfb2f6bb3
Ryabikove <lodogatze@mail.ru>
2026-08-26 20:56:57 +0300

    Now it can display number of rows and columns - close #7

--- DatasetAnalysis/dataset.py
+++ DatasetAnalysis/dataset.py
@@ -2,10 +2,22 @@ import pandas as pd
 
 df = pd.read_csv('dataset.csv')
 
-def displayData(data, filename):
+
+def displayData(data: str, filename: str) -> None:
     print(data)
     with (open(filename, 'a', newline='', encoding='utf-8')) as file:
-        file.write("\n" + data)
+        file.write(data + '\n')
+
+
+class DatasetAnalysis:
+    dataset: pd.DataFrame
+    def __init__(self, dataset: pd.DataFrame) -> None:
+        self.dataset = dataset.reset_index()
+
+    def RowsNColumnsNumb(self) -> None:
+        displayData(str(self.dataset.shape), 'report.txt')
+
 
 if __name__ == '__main__':
-    displayData(df.columns.values[1], 'report.txt')
+    analysis = DatasetAnalysis(df)
+    analysis.RowsNColumnsNumb()
